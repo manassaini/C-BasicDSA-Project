2397725b5685a2ac085f6c0434dde007ad01d20d
Manas Saini <manassaini@mymail.mines.edu>
2021-07-01 14:22:15 -0600

    Added manual tree creation function

--- TreeNode.h
+++ TreeNode.h
@@ -28,4 +28,27 @@ public:
         printTree(root->left);
         printTree(root->right);
     }
+
+    TreeNode* createTreeManual(){
+        TreeNode* root = new TreeNode(1);
+        root->left = new TreeNode(2);
+        root->right = new TreeNode(3);
+        root->left->left = new TreeNode(4);
+        root->left->right = new TreeNode(5);
+        root->right->left = new TreeNode(6);
+        root->right->right = new TreeNode(7);
+        return root;
+    }
+
+    TreeNode* createTreeManual2(){
+        TreeNode* root = new TreeNode(1);
+        root->left = new TreeNode(2);
+        root->right = new TreeNode(3);
+        root->left->left = new TreeNode(4);
+        root->left->right = new TreeNode(5);
+        root->right->left = new TreeNode(6);
+        root->right->right = new TreeNode(8);
+        root->right->right->right = new TreeNode(9);
+        return root;
+    }
 };
