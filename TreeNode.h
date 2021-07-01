8dfe8821248cbc1756eae67c34d5e7e8bdf629f3
Manas Saini <manassaini@mymail.mines.edu>
2021-07-01 14:25:30 -0600

    Added postOrder function

--- TreeNode.h
+++ TreeNode.h
@@ -86,4 +86,13 @@ public:
         cout << root->data << " ";
         inOrder(root->right);
     }
+
+    void postOrder(TreeNode* root){
+        if (root == NULL){
+            return;
+        }
+        postOrder(root->left);
+        postOrder(root->right);
+        cout << root->data << " ";
+    }
 };
