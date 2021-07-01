ac9bacf43436c9a00b4cb8c100d5d899779bada1
Manas Saini <manassaini@mymail.mines.edu>
2021-07-01 14:28:21 -0600

    Added isSameTree function

--- TreeNode.h
+++ TreeNode.h
@@ -120,4 +120,19 @@ public:
         }
         return root;
     }
+
+    bool isSameTree(TreeNode* root1, TreeNode* root2){
+        if (root1 == NULL && root2 == NULL) {
+            return true;
+        }
+        else if (root1 == NULL || root2 == NULL) {
+            return false;
+        }
+        else if (root1->data != root2->data) {
+            return false;
+        }
+        else{
+            return isSameTree(root1->left, root2->left) && isSameTree(root1->right, root2->right);
+        }
+    }
 };
