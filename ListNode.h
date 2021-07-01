0bcb914ebad2b54b87f47c4cdbd74f986fa2f8f5
Manas Saini <manassaini@mymail.mines.edu>
2021-07-01 12:51:19 -0600

    Finished showAddNodeToFrontCode function

--- ListNode.h
+++ ListNode.h
@@ -106,6 +106,23 @@ public:
         return head;
     }
 
+    void showAddNodeToFrontCode(){
+        cout << "Node* addNodeToFront(Node* head, int num){" << endl;
+        cout << "    int newData;" << endl;
+        cout << "    Node* temp;" << endl;
+        cout << "    Node* newNode;" << endl;
+        cout << "    temp = head;" << endl;
+        cout << "    for (int i = 0; i < num; ++i){" << endl;
+        cout << "        cout << ""Enter data for your new node:";"" << endl;
+        cout << "        cin >> newData;" << endl;
+        cout << "        newNode = createNewNode(newData);" << endl;
+        cout << "        newNode->next = head;" << endl;
+        cout << "        head = newNode;" << endl;
+        cout << "    }" << endl;
+        cout << "    return head;" << endl;
+        cout << "}" << endl;
+    }
+
     Node* findSpecificNode(Node* node, int value){
         while (node != NULL){
             if (node->data == value) return node;
