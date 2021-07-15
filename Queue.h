1e49f189cd50940b5de71041472033cc57d4565b
Manas Saini <manassaini@mymail.mines.edu>
2021-07-14 22:34:52 -0600

    leaving modifyQueue functions as it is, outputs are fine

--- Queue.h
+++ Queue.h
@@ -175,6 +175,7 @@ public:
         cout << endl << "Given an integer K and a queue of integers, we need to reverse the order of"
                         "the first K elements of the queue, leaving the other elements in the same relative order." << endl;
         cout << "*** COURTESY OF GEEKSFORGEEKS ***";
+        cout << "All good";
     }
 
     void showExamplesModifyQueue(){
